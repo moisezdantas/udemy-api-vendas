6ba9704338c24fc84e1e9699122446b51c5d83a4
Moisez Dantas <moisez.dantas@visto.global>
2021-07-29 11:23:58 -0300

    add token user

--- src/modules/users/services/CreateSessionsService.ts
+++ src/modules/users/services/CreateSessionsService.ts
@@ -3,14 +3,18 @@ import { getCustomRepository } from 'typeorm';
 import AppError from '@shared/errors/AppError';
 import User from '../typeorm/entities/User';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 
 interface IRequest {
   email: string;
   password: string;
 }
-
+interface IResponse {
+  user: User;
+  token: string;
+}
 class CreateSessionsService {
-  public async execute({ email, password }: IRequest): Promise<User> {
+  public async execute({ email, password }: IRequest): Promise<IResponse> {
     const usersRepository = getCustomRepository(UsersRepository);
 
     const user = await usersRepository.findByEmail(email);
@@ -25,7 +29,15 @@ class CreateSessionsService {
       throw new AppError('Incorrect email/password combination.', 401);
     }
 
-    return user;
+    const token = sign({}, '3c4ec8686c55104ae6f09241c00e7a64', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
+    return {
+      user,
+      token,
+    };
   }
 }
 
