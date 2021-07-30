6bdbdccce530ed2e061ff18247de25e4be65bc49
Moisez Dantas <moisez.dantas@visto.global>
2021-07-30 12:22:46 -0300

    add service resetpassword, forgotpassword

---
+++ src/modules/users/services/ResetPaswordService.ts
@@ -0,0 +1,39 @@
+import AppError from '@shared/errors/AppError';
+import { isAfter, addHours } from 'date-fns';
+import { UserTokensRepository } from './../typeorm/repositories/UsersTokensRepository';
+import { UsersRepository } from './../typeorm/repositories/UsersRepository';
+import { getCustomRepository } from 'typeorm';
+import { hash } from 'bcryptjs';
+
+interface IRequest {
+  token: string;
+  password: string;
+}
+
+export default class ResetPasswordService {
+  public async execute({ token, password }: IRequest): Promise<void> {
+    const usersRepository = getCustomRepository(UsersRepository);
+    const userTokenRepository = getCustomRepository(UserTokensRepository);
+
+    const userToken = await userTokenRepository.findByToken(token);
+
+    if (!userToken) {
+      throw new AppError('User Token does not exists.');
+    }
+
+    const user = await usersRepository.findById(userToken.user_id);
+
+    if (!user) {
+      throw new AppError('User does not exists.');
+    }
+
+    const tokenCreateAt = userToken.created_at;
+    const compareDate = addHours(tokenCreateAt, 2);
+
+    if (isAfter(Date.now(), compareDate)) {
+      throw new AppError('Token expired.');
+    }
+
+    user.password = await hash(password, 8);
+  }
+}
---
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -0,0 +1,25 @@
+import AppError from '@shared/errors/AppError';
+import { UserTokensRepository } from './../typeorm/repositories/UsersTokensRepository';
+import { UsersRepository } from './../typeorm/repositories/UsersRepository';
+import { getCustomRepository } from 'typeorm';
+
+interface IRequest {
+  email: string;
+}
+
+export default class SendForgotPasswordEmailService {
+  public async execute({ email }: IRequest): Promise<void> {
+    const usersRepository = getCustomRepository(UsersRepository);
+    const userTokenRepository = getCustomRepository(UserTokensRepository);
+
+    const user = await usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new AppError('User does not exists.');
+    }
+
+    const token = await userTokenRepository.generate(user.id);
+
+    console.log(token);
+  }
+}
